cf3dcb26c1f07185f5c9bdfd9e8a7b7058b4de31
JodiMcJodson <isekgood@gmail.com>
2023-01-08 21:51:05 -0600

    Modified-by: JodiMcJodson <isekgood@gmail.com>
    	new file:   .gitignore
    	modified:   config.h

--- config.h
+++ config.h
@@ -1,10 +1,8 @@
-
-#define SHCMD(cmd)                                                             \
-  { "/bin/sh", "-c", cmd, NULL }
-
 static const char datecmd[] = "date";
 static const char hostnamecmd[] = "hostname";
-static const char ramcmd[] = "free -h | awk 'FNR == 2 {print $3 \" / \"$2}'";
+static const char ramcmd[] = "free -h | awk 'FNR == 2 {print $3 \"/\"$2}'";
+static const char cpucmd[] = "vmstat | awk 'FNR == 3 {print 100-$15\"%\"}'";
+static const char tempcmd[] = "sensors | sed -En 's/^temp.: *\\+(.*C) /\\1/p' | awk 'FNR == 1' | sed -En 's/crit = \\+//p'";
 #define seperator " | "
 
 #define SECONDS(n) (1 * 1000)
@@ -15,4 +13,6 @@ static const Module modules[] = {
     {NULL, hostnamecmd, 0},
     {NULL, datecmd, SECONDS(1)},
     {"RAM: ", ramcmd, MINUTES(1.5)},
+		{"CPU: ", cpucmd, MINUTES(0.5)},
+		{"TEMP: ", tempcmd, MINUTES(0.5)},
 };
